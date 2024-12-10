7163251aa8f017f3e326b977aa6d7dfeb3c3235f
Saturn Computing Topic <hatsanmobiletopic@gmail.com>
2024-12-10 21:06:32 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from .PyGodot import PyGodotRunVS,PyGodotRunGD
+import os
+
+os.system("pip install pygodot")
+def functions(Data):
+    if Command1 == "PyGodot.RunVS(.vs)":
+        print("Running")
+    if Command2 == "PyGodot.RunGD(.gd)":
+        print("Running")
+
+
+
---
+++ pygodot.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('ping youtube.com')
+def Functions(__init__):
+    import pygodot
